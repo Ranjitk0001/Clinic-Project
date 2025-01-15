3b49b24695927f7612c934dfdfa5aa3bafff510c
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2025-01-15 12:53:56 +0530

    Spring-boot :- Create a project
    Run Succefully

---
+++ Hospiatal Managment System/hospitalManagementSystemBackEnd/src/main/java/com/peakprosysproject/hospitalManagementSystemBackEnd/HospitalManagementSystemBackEndApplication.java
@@ -0,0 +1,14 @@
+package com.peakprosysproject.hospitalManagementSystemBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementSystemBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagementSystemBackEndApplication.class, args);
+		System.out.println("Hospital Management System BackEnd");
+	}
+
+}
